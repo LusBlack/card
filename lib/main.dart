ddc255033b2c5b31757fe206f24b8e31a1563906
Louis <Lou.ikwuegbu@gmail.com>
2025-09-02 19:40:27 +0100

    checking out column layouts

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,43 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                padding: EdgeInsets.all(10.0),
+                color: Colors.blue,
+                child: Text('Container 2')
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.red,
+                child: Text('Container 3'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*
+appBar: AppBar(
           title: Center(
             child: Text(
               'Iron Within, Iron Without',
@@ -19,7 +54,4 @@ class MyApp extends StatelessWidget {
           ),
           backgroundColor: Colors.black,
         ),
-      ),
-    );
-  }
-}
+ */
