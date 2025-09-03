63a52865b5ee4c139223220faae919111a0e183a
Louis <Lou.ikwuegbu@gmail.com>
2025-09-03 21:47:38 +0100

    Added profile image

--- lib/main.dart
+++ lib/main.dart
@@ -13,45 +13,16 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: <Widget>[
-              Container(
-                height: 100.0,
-                width: 100.0,
-                margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
-                padding: EdgeInsets.all(10.0),
-                color: Colors.white,
-                child: Text('Container 1'),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('Images/Lu.jpg'),
               ),
-              Container(
-                width: 100,
-                height: 100,
-                padding: EdgeInsets.all(10.0),
-                color: Colors.blue,
-                child: Text('Container 2')
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
-                padding: EdgeInsets.all(10.0),
-                color: Colors.red,
-                child: Text('Container 3'),
-              ),
-            ],
-          ),
+            ]
+          )
         ),
       ),
     );
   }
 }
 
-/*
-appBar: AppBar(
-          title: Center(
-            child: Text(
-              'Iron Within, Iron Without',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-          backgroundColor: Colors.black,
-        ),
- */
+
