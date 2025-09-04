5ccc5792f76a242b81356574c3be0c9b97dccfd5
Louis <Lou.ikwuegbu@gmail.com>
2025-09-04 22:12:32 +0100

    added phone and email icons to contact card

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,88 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage('Images/Lu.jpg'),
               ),
-            ]
-          )
+
+              Text(
+                'Lezie',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Android Dev',
+                style: TextStyle(
+                  fontFamily: 'SourceCodePro',
+                  fontSize: 20,
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              // ignore: avoid_unnecessary_containers
+
+              //Call info
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                //padding: EdgeInsets.all(10.0),
+                child: Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.phone, color: Colors.teal),
+                      SizedBox(
+                          width: 10
+                      ),
+                      Text(
+                        '+234 123456789',
+                        style: TextStyle(
+                          fontFamily: 'SourceCodePro',
+                          color: Colors.teal[900],
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 2.5,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              //Email info
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                //padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.email, color: Colors.teal),
+                    SizedBox(
+                        width: 10
+                    ),
+                    Text(
+                      'ligmaballs@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'SourceCodePro',
+                        color: Colors.teal[900],
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 2.5,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
